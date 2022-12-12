4136ebbe2125608af8c1491382632372a83a3a84
senapedro <ph997595305@gmail.com>
2022-12-12 00:14:33 -0300

    Primeiro commit desse codigo

---
+++ public/js/api.js
@@ -0,0 +1,15 @@
+// let api_key = "b12fd4a6382cb6e29d1cbc7a752138bf"
+
+// let img_url = "https://image.tmdb.org/t/p/w500"
+
+// let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+// let movie_genres_http = "https://api.themoviedb.org/3/discover/list?"
+
+let api_key = "d19a1946970f98fae002af7545322879"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
